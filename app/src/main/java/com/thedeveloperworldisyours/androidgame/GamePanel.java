10e90e2a9f7c9252a3f9393ff5815df342f9fd5e
Cabezas <javiergonzalezcabezas@gmail.com>
2017-04-08 08:54:43 +0100

    first commit

---
+++ app/src/main/java/com/thedeveloperworldisyours/androidgame/GamePanel.java
@@ -0,0 +1,12 @@
+package com.thedeveloperworldisyours.androidgame;
+
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by javiergonzalezcabezas on 8/4/17.
+ */
+
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
+
+}
---
+++ app/src/main/java/com/thedeveloperworldisyours/androidgame/MainActivity.java
@@ -0,0 +1,25 @@
+package com.thedeveloperworldisyours.androidgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    GamePanel mPanel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //turn title off
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //set to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        mPanel = new GamePanel(this);
+
+        setContentView(mPanel);
+    }
+}
